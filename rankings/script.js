7f184f772060dcc9eeb78a224d79d5a28e0b7cb2
Paul Pedrozo <pedrozop@amazon.com>
2023-11-10 14:37:39 -0600

    edit styling and added No Results functionality when filter doesn't return anything

--- rankings/script.js
+++ rankings/script.js
@@ -1,19 +1,35 @@
 function filterMovies() {
   const searchQuery = document.getElementById('search-bar').value.toLowerCase();
   const selectedRating = document.getElementById('rating-filter').value;
+  let x = document.createElement("H3");
+  let t = document.createTextNode("No Results");
+  x.setAttribute("id","nores");
+  x.appendChild(t);
+  const ref = document.getElementById("foot");
 
   const movieList = document.querySelectorAll('#movie-list li');
+  let count = 0;
   movieList.forEach((movieItem) => {
     const movieName = movieItem.innerText.toLowerCase();
     const movieRating = parseInt(movieItem.getAttribute('data-rating'));
-
+    
     if (movieName.includes(searchQuery) && (selectedRating === "all" || movieRating >= parseInt(selectedRating))) {
       movieItem.style.display = 'list-item';
+      if (document.getElementById("nores") != null) {
+        document.getElementById("nores").remove();
+      }
     } else {
       movieItem.style.display = 'none';
+      count ++;
     }
   });
+  if (movieList.length == count){
+    if (document.getElementById("nores") == null){
+      ref.parentNode.insertBefore(x, ref);
+    }
+  }
 }
+
 function generateStars() {
   const movieList = document.querySelectorAll('#movie-list li');
   movieList.forEach((movieItem) => {
